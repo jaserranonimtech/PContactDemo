3aa4afe3ae50e0e69d5ccc2d162b84798ab6faba
Jose Antonio Serrano <jaserrano@nimtech.com.mx>
2018-09-10 21:47:32 -0500

    Se agrega el seed metod de un usuario adminstrador

--- PContact/PContactDemo/PContactDemo/Controllers/HomeController.cs
+++ PContact/PContactDemo/PContactDemo/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace PContactDemo.Controllers
     {
         public ActionResult Index()
         {
+
             return View();
         }
 
--- PContact/PContactDemo/PContactDemo/Migrations/Configuration.cs
+++ PContact/PContactDemo/PContactDemo/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace PContactDemo.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using PContactDemo.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +17,47 @@ namespace PContactDemo.Migrations
 
         protected override void Seed(PContactDemo.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            try
+            {
+                //Roles de Administración de sistema 
+                context.Roles.AddOrUpdate(a => a.Id, new IdentityRole
+                {
+                    Id = "c26a8b05-f431-4458-97d8-966209f8c2ee",
+                    Name = "ADMINISTRATOR"
+                });
+
+                context.Users.AddOrUpdate(a => a.Id, new Models.ApplicationUser
+                {
+                    Id = "ac55797d-f2ed-499f-ad56-2fadeac9eac4",
+                    Email = "admin@nimtech.com.mx",
+                    PasswordHash = new PasswordHasher().HashPassword("Password1."),
+                    EmailConfirmed = false,
+                    PhoneNumberConfirmed = false,
+                    LockoutEnabled = true,
+                    TwoFactorEnabled = false,
+                    AccessFailedCount = 0,
+                    UserName = "admin@nimtech.com.mx",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    NombreCompleto = "Administrador"
+                });
+
+                var userAdmin = context.Set<ApplicationUser>().Find("ac55797d-f2ed-499f-ad56-2fadeac9eac4");
+                if (!userAdmin.Roles.Any())
+                {
+                    userAdmin.Roles.Add(new IdentityUserRole
+                    {
+                        RoleId = "c26a8b05-f431-4458-97d8-966209f8c2ee",
+                        UserId = "ac55797d-f2ed-499f-ad56-2fadeac9eac4"
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message, e.InnerException);
+            }
+          
         }
     }
 }
