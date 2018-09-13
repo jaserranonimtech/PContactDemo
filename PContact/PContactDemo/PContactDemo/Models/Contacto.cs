833a32dfe9cb9600fa576a9bf3ffbf9434c94147
Nico <bttf-isback@hotmail.com>
2018-09-12 22:32:45 -0300

    Se añadió nuevos modelos
    Se agrego los modelos de acuerdo al diagrama DER

---
+++ PContact/PContactDemo/PContactDemo/Models/Contacto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PContactDemo.Models
+{
+    public class Contacto
+    {
+        //atributos
+        
+        public int  IdContacto { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [Required]
+        public string Telefono { get; set; }
+        public string Telefeno2 { get; set; }
+        [Required]
+        public string CorreoElectronico { get; set; }
+        public string CorreoElectronico2 { get; set; }
+        [Required]
+        public bool Activo { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public DateTime FechaCreacion  { get;  set; }
+        public string UsuarioModificacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        [Required]
+        public TipoContacto TipodeContacto { get; set; }
+        public ContactoDireccion ContactoDirecion { get; set; }
+
+
+    }
+}
---
+++ PContact/PContactDemo/PContactDemo/Models/ContactoDireccion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PContactDemo.Models
+{
+    public class ContactoDireccion
+    {
+      
+        public int IdContactoDireccion { get; set; }
+        [Required]
+        public string Calle { get; set; }
+
+        public string Colonia { get; set; }
+        [Required]
+        public string Estado { get; set; }
+        [Required]
+        public string Ciudad { get; set; }
+        public string Delegacion { get; set; }
+
+        [Required]
+        public string CodigoPostal { get; set; }
+        public string Referencia { get; set; }
+        public string NoInt { get; set; }
+        public string NoExt { get; set; }
+        
+
+    }
+}
